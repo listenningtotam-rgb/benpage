a3e29fb42e223060e5645c513fa3c4310fc17a4e
bentan <listenning_to@hotmail.com>
2026-07-30 16:55:24 +0800

    fix: hardcode port to 3000, remove PORT env support

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const publicDir = path.join(__dirname, "public");
 
 const mimeTypes = {
